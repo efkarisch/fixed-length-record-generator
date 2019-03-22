0096060f878cf6b2669672b7fc53e8fd3b716723
Ed Karisch <edward.karisch@getcsi.com>
2019-03-22 13:30:49 -0500

    First working and tested draft with mock data

--- index.js
+++ index.js
@@ -62,6 +62,8 @@ privateObj.DEFAULT_OPTIONS = { //These are the minimum defaults for the program 
 
 //Find and return unique values against comparator (b)
 Array.prototype.unique = function(b){
+    let z = this;
+    let y = b;
 
     let aLen = this.length,
         len  = b.length;
@@ -77,17 +79,19 @@ Array.prototype.unique = function(b){
         return gather(b,this);
     }
 
-    function gather(needleArr,haystackArr){
+    function gather(needleArr,haystackArr, reduce){
 
         let uniqueArr = [];
 
         while(len){
 
-            let curVal = needleArr.pop();
+            //may not want to reduce original arrays
+            let curVal = reduce ? needleArr.pop() :  needleArr[len - 1];
 
             if(haystackArr.indexOf(curVal) > -1 && uniqueArr.indexOf(curVal) < 0)
                 uniqueArr.push(curVal);
             len -= 1;
+
         }//loop
 
         return uniqueArr;
@@ -113,12 +117,6 @@ String.prototype.justify = function(char, times, pos){
 };
 
 
-
-//THIS IS ONLY FOR DEVELOPMENT
-if(!prod)privateObj.runTest();
-
-
-
 /********************
  * Functions:
  ********************/
@@ -142,7 +140,7 @@ privateObj.runTest = function() {
                     name: 'First',
                     width: 20,
                     defaultValue: '',
-                    prepad: 5,
+                    prepad: 0,
                 },
                 {
                     name: 'Last',
@@ -155,36 +153,37 @@ privateObj.runTest = function() {
                     justify: 'right',
                     defaultValue: '',
                     padchar: '*',
-                    postpad: 30,
+                    postpad: 0,
                 }
             ]
     };
 
     dataArrOfArrays = [
+
         [
-          "First",
           "Last",
+          "First",
           "Email"
         ],
         [
-            "John",
             "Munson",
+            "John",
             "JaysMunchin@gitmail.com"
         ],
         [
-            "Ed",
             "Karisch",
+            "Ed",
             "SpecialEd@gitmail.com"
         ],
         [
-            "Josh",
             "Stevens",
+            "Josh",
             "JoshCapone@gitmail.com"
         ]
 
     ];
 
-    flrData  = buildFLR(options, dataArrOfArrays);
+    let flrData  = publicObj.buildFLR(options, dataArrOfArrays);
 
     console.log(flrData);
 
@@ -214,8 +213,8 @@ privateObj.initOptions = function(opts){
     //number of fields = number of fields in schema
     privateObj.fieldCT = opts.schema.length;
 
-    //schema loop
-    for(var i = 0; i < max; i++){
+        //schema loop
+    for(let i = 0; i < privateObj.fieldCT; i++) {
 
         //defaults loop
         for( property in opts.defaults){ //property = property from defaults
@@ -238,31 +237,61 @@ privateObj.initOptions = function(opts){
             }
 
         }//End defaults loop
+
         let fieldName = opts.schema[i]['name'];
         privateObj.fields.push(fieldName);
-        privateObj.schemaMap[i] = fieldName; //used if header row is provided, else unused
-        opts.schema[i].valueAtIndex = [i];
+        privateObj.schemaMap[fieldName] = i; //used if header row is provided, else unused
+        opts.schema[i].valueAtIndex = i;
 
-        //Now populate schema with correct value index
+    }//End first schema loop
+
+    if(!prod)console.log('Value mappings related to defined fields in schema:\n', privateObj.schemaMap);
+
+        //Now begin section to populate schema with correct value index
         let headers     = dataArrOfArrays[0];
         let rowLength   = headers.length; //could be data row and not header
 
+
         if(rowLength !== privateObj.fieldCT) privateObj.handleError('Data mismatch! Invalid schema contains ' + fieldCT + ' fields. Please add missing fields to schema.');
 
-        headers = headers.unique(privateObj.fields);
+        if(!prod)console.log('[headers] [schemafields]',headers,privateObj.fields);
+
+        let fields = privateObj.fields;
+
+        headers = headers.unique(fields);
+
+        //if(!prod)console.log('fields & privateObj.fields:',fields,privateObj.fields);
+
         rowLength = headers.length; //re-evaluate
+
+        //if(!prod)console.log('headers',headers,'first row check results h=vCT?', privateObj.fieldCT,rowLength);
 
         if(rowLength && rowLength !== privateObj.fieldCT) privateObj.handleError('Data mismatch! Invalid schema check to make sure schema name properties for fields matches with data headers (first row of data). Please make sure necessary values are exactly the same');
 
+
         if(rowLength){ //map values in schema, headers were passed in and verified (otherwise default valueAtIndex property was already set to match schema index
 
-            dataArrOfArrays.shift(); //remove headers from data (no longer needed)
+            headers = dataArrOfArrays.shift(); //remove headers from data (no longer needed)
 
-            for(var i = 0; i < privateObj.fieldCT; i++){
-                opts.schema[i].valueAtIndex =  headers.indexOf(privateObj.schemaMap[i]);
+            /**
+             UNSURE IF THIS IS NEEDED \\@todo DELETE THIS?
+            for(let j = 0; j < privateObj.fieldCT; j++){
+
+                console.log('header:', headers[j]);
+
+                console.log('valueAtIndex:', opts.schema[j].valueAtIndex);
+
+                console.log('field from fields private arr', privateObj.fields[j]);
+
+
+
+                //opts.schema[j].valueAtIndex =  headers.indexOf(privateObj.fields[j]);
             }
+             **/
+
         }
-    }//End schema loop
+
+    //if(!prod) console.log('initialized options',opts);
 
     return opts;
 
@@ -270,7 +299,7 @@ privateObj.initOptions = function(opts){
 
 
 //@todo In AWS Lambda, or other cases, you may not want to throw errors, if so, determine what to do here:
-privateOjb.handleError = function(msg){
+privateObj.handleError = function(msg){
     throw msg;
 };//handleError()
 
@@ -289,42 +318,54 @@ publicObj.buildFLR = function(options, dataArrOfArrays){
 
     let len = dataArrOfArrays.length;
 
+    if(!prod)console.log('Processing ' + len + ' rows of data' );
+
     let sb = new StringBuilder();
 
-    while(len){ //loop through row of data
+    for(let i = 0; i < len; i++) { //loop through row of data
+
+        privateObj.currentRow = (i+1);
 
         let row = dataArrOfArrays.pop();
 
-            for(var i = 0; i <  privateObj.fieldCT; i++){ //loop through schema to determine transformations
+            for(var j = 0; j <  privateObj.fieldCT; j++){ //loop through schema to determine transformations
 
                 //for schema reference:
             //* @array { schema: *[{ name: string, width: int(1), defaultValue: string, padchar: string, justify: string(left), prepad: int, postpad: int}, 'valueAtIndex: int(auto-populated)] }
                 //
-                let justPos  = options.schema[i].justify, //only need to match first char L or R.
-                    width    = options.schema[i].width,
-                    defVal   = options.schema[i].defaultValue,
-                    prePad   = options.schema[i].prepad,
-                    postPad  = options.schema[i].postpad,
-                    dataVal  = options.schema[i].valueAtIndex,
-                    padChar  = options.schema[i].padchar,
+                let justPos  = options.schema[j].justify, //only need to match first char L or R.
+                    width    = options.schema[j].width,
+                    defVal   = options.schema[j].defaultValue,
+                    prePad   = options.schema[j].prepad,
+                    postPad  = options.schema[j].postpad,
+                    dataVal  = row[options.schema[j].valueAtIndex],
+                    padChar  = options.schema[j].padchar,
                     override = options.defaults.valueOverride;
 
-                if(override) dataVal = defVal; //pre-build operation
+                //if(!prod)console.log('justPos,width,defVal,prePad,postPad,dataVal,padChar,override:',justPos,width,defVal,prePad,postPad,dataVal,"'"+padChar+"'",override );
+
+                if(override) dataVal = defVal ? defVal : dataVal; //pre-build operation
+                dataVal = dataVal.substr(0,width);
 
                 if(prePad) sb.append(padChar.padStart(prePad, padChar));//first build operation: ALWAYS
 
-                sb.append(dataVal.justify(padChar,width,justPos)); //build field data
+                sb.append(String(dataVal).justify(padChar,width,justPos)); //build field data
 
                 if(postPad) sb.append(padChar.padEnd(postPad,padChar));//last build operation: ALWAYS
 
-            }
-            //create new line for new record
-            sb.append(String.fromCharCode(13));
+            }// End looping through schema
 
-        len -= 1;
+            //create new line for new record/row
+
+            if(!prod)console.log('done with row: ' + privateObj.currentRow);
+            if(privateObj.currentRow !== privateObj.fieldCT ) sb.append( String('\n') );
+
     }//end looping through rows of data
 
     return sb.toString('',true);
 };//buildFLR()
 
+
+if(!prod)module.exports = privateObj;
+else
 module.exports = publicObj;
---
+++ test.js
@@ -0,0 +1,3 @@
+const FLRLib = require('./index.js');
+
+FLRLib.runTest();
